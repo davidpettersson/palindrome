cc718f2fbb97c9d0125addd87adcc855bde4c641
David Pettersson <david.pettersson@tetrapak.com>
2019-06-25 16:34:35 -0400

    Returned to model-level palindrome-checking. Found that the problem was that the import was removed by the IDE, so new hard dependency introduced to avoid the problem.

--- minim/apps.py
+++ minim/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 
 
+
 class MinimConfig(AppConfig):
     name = 'minim'
     verbose_name = 'Minim Application'
 
     def ready(self):
-        print('READY')
+        # Added here to ensure that the signals module is loaded and the
+        # dependency not removed by the IDE when performing import optimization.
+        from minim.signals import palindrome_checker
+        _ = palindrome_checker
