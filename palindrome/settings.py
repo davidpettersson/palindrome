9f7374f41a81ddaff825d9add14c91574b7d7e4f
David Pettersson <david.pettersson@tetrapak.com>
2019-06-25 10:19:56 -0400

    Added the Django REST framework. Removed admin application to reduce complexity.

--- palindrome/settings.py
+++ palindrome/settings.py
@@ -17,12 +17,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'minim',
 ]
 
