6250bf0cb1cab61ade72009ad316a2ca4ea39ce7
David Pettersson <david.pettersson@tetrapak.com>
2019-06-25 16:32:58 -0400

    Removed debug printout.

--- minim/signals.py
+++ minim/signals.py
@@ -7,6 +7,5 @@ from minim.palindrome import is_strict_palindrome, is_relaxed_palindrome
 
 @receiver(pre_save, sender=Message)
 def palindrome_checker(sender, instance, **kwargs):
-    print("SIGNAL!")
     instance.is_strict_palindrome = is_strict_palindrome(instance.message)
     instance.is_relaxed_palindrome = is_relaxed_palindrome(instance.message)
