4991ff99124cbd7e8db06753d0d2793cc4adae8b
David Pettersson <david.pettersson@tetrapak.com>
2019-06-25 10:49:27 -0400

    Better responsiblity split between router and URL.

--- minim/urls.py
+++ minim/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from minim import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.MessageViewSet)
+router.register(r'messages', views.MessageViewSet)
 
 urlpatterns = [
-    path('api/messages/', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
